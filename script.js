95b0ecffa72f682f03f3f417635a1d13caefcc9c
Analia <ana_292_1@hotmail.com>
2024-04-30 00:07:37 -0300

    arreglos finales

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const btnMenuHamburguesa = document.getElementById("btn_menu_hamburguesa");
 const btnCerrarMenu = document.getElementById("btn_cerrar_menu");
 const navbar = document.getElementById("navbar");
 
-//funcionalidad menu hamburguesa
+//funcionalidad menu hamburguesa--------------------------------------------------------------
 btnMenuHamburguesa.addEventListener("click", () => {
 	navbar.classList.add("visible");
 });
@@ -10,7 +10,6 @@ btnMenuHamburguesa.addEventListener("click", () => {
 btnCerrarMenu.addEventListener("click", () => {
 	navbar.classList.remove("visible");
 });
-
 
 //form ----------------------------------------------------------------------------------------
 
@@ -21,15 +20,15 @@ const message = document.getElementById("mensaje");
 const person = document.getElementById("integrante");
 const checkbox = document.getElementById("check");
 
-function errorHandler (value) {
-	value.nextElementSibling.textContent= "el campo no puede estar vacio"
-	value.classList.remove("rm-error")
-	value.classList.add("error")	
+function errorHandler(value) {
+	value.nextElementSibling.textContent = "el campo no puede estar vacio";
+	value.classList.remove("rm-error");
+	value.classList.add("error");
 }
 
 submitForm.addEventListener("submit", (event) => {
-	event.preventDefault();	
-	alert('Mensaje enviado con exito...')
+	event.preventDefault();
+	alert("Mensaje enviado con exito...");
 	n.value = "";
 	errorHandler(n);
 	mail.value = "";
@@ -42,21 +41,21 @@ submitForm.addEventListener("submit", (event) => {
 	errorHandler(checkbox);
 });
 
-const camposFormulario = document.querySelectorAll("[required]")
+const camposFormulario = document.querySelectorAll("[required]");
 
-camposFormulario.forEach((campo)=>{
-	campo.addEventListener("blur",()=> verificarCampor(campo))
-	campo.addEventListener("invalid", evento => evento.preventDefault())
-})
+camposFormulario.forEach((campo) => {
+	campo.addEventListener("blur", () => verificarCampor(campo));
+	campo.addEventListener("invalid", (evento) => evento.preventDefault());
+});
 
-function verificarCampor(campo){
-	if(campo.validity.valueMissing == false){
-		const errorMenssage= campo.nextElementSibling
-		errorMenssage.textContent= ""
-		campo.classList.remove("error")
-		campo.classList.add("rm-error")
-		console.log('estoy en valueMissin');
-	}else {
-		errorHandler(campo)
+function verificarCampor(campo) {
+	if (campo.validity.valueMissing == false) {
+		const errorMenssage = campo.nextElementSibling;
+		errorMenssage.textContent = "";
+		campo.classList.remove("error");
+		campo.classList.add("rm-error");
+		console.log("estoy en valueMissin");
+	} else {
+		errorHandler(campo);
 	}
 }
